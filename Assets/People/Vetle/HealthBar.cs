f2dc2571541f4723059ff920df4131e5e84f4695
Vetlish <vewi009@student.kristiania.no>
2023-03-10 07:33:04 +0100

    forsøk på lerp + fiksa script

--- Assets/People/Vetle/HealthBar.cs
+++ Assets/People/Vetle/HealthBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,81 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider slider;
-    public float increaseSpeed;
-    public HealthMan healthMan;
+    //public float minimum;
 
-    public void SetMaxHealth(int health)
+    //public Image mask;
+    //public Image fill;
+
+    //public float maximumHealth = 100f;
+    //public float currentHealth;
+
+    float lerpSpeed;
+    public int maxHealth = 10;
+    public int minHealth = 0;
+    public int currentHealth;
+    public int damageNumber = 5;
+
+    public Slider slider;
+
+    private void Start()
     {
-        slider.maxValue = health;
-        slider.value = health;
+        currentHealth = maxHealth;
+        SetMaxHealth();
     }
-    public void SetHealth(int health)
+
+    private void Update()
     {
-        slider.value = health;
+        if (currentHealth > maxHealth) currentHealth = maxHealth;
+        if (currentHealth < minHealth) currentHealth = minHealth;
+        lerpSpeed = 3f * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DamageTaken(damageNumber);
+        }
     }
+
+    private void DamageTaken(int damage)
+    {
+        currentHealth -= damage;
+        SetHealth();
+    }
+
+    public void SetMaxHealth()
+    {
+        slider.maxValue = maxHealth;
+        //slider.value = health;
+    }
+    public void SetHealth()
+    {
+        slider.value = currentHealth;
+        //float sliderValue = currentHealth / maxHealth;
+        //slider.value = Mathf.Lerp(slider.value, damageNumber, lerpSpeed);
+    }
+
+    // void Update()
+    //  {
+    //  currentHealth = Mathf.Clamp(currentHealth, 0, maximumHealth);
+    //  if (currentHealth > maximumHealth) currentHealth = maximumHealth;
+
+    // GetCurrentFill();
+    // if (Input.GetKeyDown(KeyCode.Space))
+    // {
+    // TakeDamage(5);
+    // }
+    // }
+
+    // private void GetCurrentFill()
+    // {
+    //float currentOffset = currentHealth - minimum;
+    //float maximumOffset = maximumHealth - minimum;
+
+    // float fillAmount = currentOffset / maximumOffset;
+    // mask.fillAmount = Mathf.Lerp(mask.fillAmount, fillAmount, lerpSpeed);
+    // }
+
+    //public void TakeDamage(float damage)
+    // {
+    //currentHealth -= damage;
+    //}
 }
--- Assets/People/Vetle/ManaBar.cs
+++ Assets/People/Vetle/ManaBar.cs
@@ -5,16 +5,48 @@ using UnityEngine.UI;
 
 public class ManaBar : MonoBehaviour
 {
+
+    float lerpSpeed;
+    public int maxMana = 10;
+    public int minMana = 0;
+    public int currentMana;
+    public int usageNumber = 5;
+
     public Slider slider;
 
-    public void SetMaxMana(int mana)
+    private void Start()
     {
-        slider.maxValue = mana;
-        slider.value = mana;
-    }
-    public void SetHealth(int mana)
-    {
-        slider.value = mana;
+        currentMana = maxMana;
+        SetMaxMana();
     }
 
+    private void Update()
+    {
+        if (currentMana > maxMana) currentMana = maxMana;
+        if (currentMana < minMana) currentMana = minMana;
+        lerpSpeed = 3f * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DamageTaken(usageNumber);
+        }
+    }
+
+    private void DamageTaken(int usage)
+    {
+        currentMana -= usage;
+        SetMana();
+    }
+
+    public void SetMaxMana()
+    {
+        slider.maxValue = maxMana;
+        //slider.value = health;
+    }
+    public void SetMana()
+    {
+        slider.value = currentMana;
+        //float sliderValue = currentHealth / maxHealth;
+        //slider.value = Mathf.Lerp(slider.value, damageNumber, lerpSpeed);
+    }
 }
