a03d34f9698e33ef9b555ffb1a30728d0a0a3a47
Viktor <viktormmeliassen@gmail.com>
2023-03-21 21:46:36 +0100

    qol

--- Assets/People/Viktor/Better Prototype/Scripts/Movement.cs
+++ Assets/People/Viktor/Better Prototype/Scripts/Movement.cs
@@ -8,7 +8,7 @@ public class Movement : MonoBehaviour
 
     private float horizontal;
     public float vertical;
-    public float speed = 8f;
+    public float speed = 5f;
     public float jumpingPower = 16f;
     public LayerMask ground;
 
@@ -36,40 +36,19 @@ public class Movement : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.y, jumpingPower);
-        }
-        //if u hold jump longer u jump higher
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-
-        //Raycast grounded check
-        bool IsGrounded()
-        {
-
-
-            Vector2 position = transform.position;
-            Vector2 direction = Vector2.down;
-            float distance = 0.8f;
-
-
-
-            Debug.DrawRay(position, direction, Color.green);
-            RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, ground);
-            if (hit.collider != null)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-
-
         
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 10f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 5f;
+        }
+
+
+
 
         //Flip();
     }
